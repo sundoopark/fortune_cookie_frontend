dea4afc6588f1223290eee75b033d86504f82674
Sundoo Park <spark129@stanford.edu>
2025-05-07 16:13:40 -0700

    refactor: enhance HTML structure in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className="light"
+      style={{ colorScheme: "light" }}
+    >
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
